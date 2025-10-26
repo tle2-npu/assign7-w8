a44ef4cade82c73f313ba0baac72bf3f9340e7df
tle2-npu <tle2@northpark.edu>
2025-10-25 19:17:36 -0500

    Added favoriteFoods array, loops, and food recommendation function

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 console.log("Hello World!");
 // 1. Create an array called favoriteFoods with at least 6 foods you love.
-
-
+let favoriteFoods = ["Pizza", "Sushi", "Ramen", "Pho", "Matcha milktea", "Chicken wings"];
 
 // 2. Loop through the list and print: "One of my favorite foods is ______."
-
-
-
-
+for (let i = 0; i < favoriteFoods.length; i++) {
+    console.log("One of my favorite foods is " + favoriteFoods[i] + ".");
+}
 
 // 3. Print out the rating for each food with a ranking like:
 // "My #1 favorite food is Ramen" (copy/paste for all items)
+for (let i = 0; i < favoriteFoods.length; i++) {
+    console.log("My #" + (i + 1) + " favorite food is " + favoriteFoods[i] + ".");
+}
 // "My #2 favorite food is Sushi"
 // ...etc.
-
-
-
 
 
 // 4a. Create a function printFoodRecommendation(foodName) that prints out the following for the foodName provided
     // "Have you ever tried ____?"
     // "I always recommend ____ to friends."
     // "Trust me — ____ is delicious."
-
-
-
+function printFoodRecommendation(foodName) {
+    console.log("Have you ever tried " + foodName + "?");
+    console.log("I always recommend " + foodName + " to friends.");
+    console.log("Trust me — " + foodName + " is delicious.");
+}
 
 // 4b. Call the function at least 3 times
-
-
+printFoodRecommendation("Matcha milktea");
+printFoodRecommendation("Pho");
+printFoodRecommendation("Sushi");
 
 // Here's a list of 50 friends' favorite foods:
 let friendFavorites = [
@@ -36,19 +37,12 @@ let friendFavorites = [
   ];
 
 // 5. Print out only foods that have an "a" in the name. For example, "Pizza" would not be included, but "Pasta" would be.
-
-
-
 // 6. Store the result in an array called foodsWithA. Print out the array.
-
-
 
 // 7. Create a new array longFoodNames for foods with names longer than 6 characters.
 
 
-
 // 8. Create another array shortFoodNames for foods 6 characters or shorter.
-
 
 
 // 9. Print both arrays and compare:
