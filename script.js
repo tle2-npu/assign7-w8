badd27ae380f4299d2eae9d1c67f1cb7eab57bc6
tle2-npu <tle2@northpark.edu>
2025-10-25 21:10:33 -0500

    Finalized practice problem 10

--- script.js
+++ script.js
@@ -63,14 +63,30 @@ for (let i = 0; i < friendFavorites.length; i++) {
 // 8. Create another array shortFoodNames for foods 6 characters or shorter.
 let shortFoodNames = [];
 for (let i = 0; i < friendFavorites.length; i++) {
-    if (friendFavorites[i].length < 6) {
+    if (friendFavorites[i].length <= 6) {
         shortFoodNames.push(friendFavorites[i]);
     }
 }
 
 // 9. Print both arrays and compare:
 // "There are more long-named foods." OR "There are more short-named foods."
+if (longFoodNames.length > shortFoodNames.length) {
+    console.log("There are more long-named foods.");
+} else if (longFoodNames.length < shortFoodNames.length) {
+    console.log("There are more short-named foods.");
+} else {
+    console.log("There are equal numbers of long and short-named foods.");
+}
 
+console.log("Long-named foods:", longFoodNames);
+console.log("Short-named foods:", shortFoodNames);
 
 // 10. STRETCH: Find the longest food name and print:
 // "The longest food name in the list is ______ with ___ characters."
+let longestFood = "";
+for (let i = 0; i < friendFavorites.length; i++) {
+    if (friendFavorites[i].length > longestFood.length) {
+        longestFood = friendFavorites[i];
+    }
+}
+console.log("The longest food name in the list is " + longestFood + " with " + longestFood.length + " characters.");
